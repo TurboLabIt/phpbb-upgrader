748298429f5cce2f8e02cd9c06757a3a8a21aa19
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-11-29 09:17:58 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo ""
+SCRIPT_NAME=phpbb-upgrader
+
+## bash-fx
+if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/master/setup.sh?$(date +%s) | sudo bash
+source /usr/local/turbolab.it/bash-fx/bash-fx.sh
+## bash-fx is ready
+
+sudo bash /usr/local/turbolab.it/bash-fx/setup/start.sh ${SCRIPT_NAME}
+fxLinkBin ${INSTALL_DIR}${SCRIPT_NAME}.sh
+
+## zzmysqldump
+sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/zzmysqldump/master/setup.sh?$(date +%s) | sudo bash
+
+sudo bash /usr/local/turbolab.it/bash-fx/setup/the-end.sh ${SCRIPT_NAME}
